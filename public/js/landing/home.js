dd19102ce2f69bc8c34e35a678165b9fd136969b
kelsonic <kgadams93@gmail.com>
2016-08-17 22:14:12 +0200

    Update footer

--- public/js/landing/home.js
+++ public/js/landing/home.js
@@ -22,7 +22,7 @@
 
         // create points
         points = [];
-        for(var x = 0; x < width; x = x + width/20) {
+        for(var x = 0; x < width; x += width/20) {
             for(var y = 0; y < height; y = y + height/20) {
                 var px = x + Math.random()*width/20;
                 var py = y + Math.random()*height/20;
